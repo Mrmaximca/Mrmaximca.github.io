9b87d28a2200d787299389f01edb6605d89921a8
Mrmaximca <Mrmaximca1337@mail.ru>
2024-12-15 23:10:52 +0700

    1-7 qwe

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -17,7 +17,6 @@ import imagemin_optipng from "imagemin-optipng";
 import svgmin from "gulp-svgmin";
 import svgstore from "gulp-svgstore";
 import server from "browser-sync";
-
 const resources = {
   html: "src/html/**/*.html",
   jsDev: "src/scripts/dev/**/*.js",
@@ -29,17 +28,16 @@ const resources = {
     "src/assets/icons/**/*.*",
     "src/assets/favicons/**/*.*",
     "src/assets/fonts/**/*.{woff,woff2}",
-    "src/assets/video/**/*.{mp4,webm}",
-    "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
-    "src/json/**/*.json",
-    "src/php/**/*.php"
+    // "src/assets/video/**/*.{mp4,webm}",
+    // "src/assets/audio/**/*.{mp3,ogg,wav,aac}",
+    // "src/json/**/*.json",
+    // "src/php/**/*.php"
   ]
 };
-
+// Gulp Tasks:
 function clean() {
   return del("dist");
 }
-
 function includeHtml() {
   return gulp
     .src("src/html/*.html")
@@ -53,7 +51,6 @@ function includeHtml() {
     .pipe(formatHtml())
     .pipe(gulp.dest("dist"));
 }
-
 function style() {
   return gulp
     .src("src/styles/styles.less")
@@ -72,7 +69,6 @@ function style() {
     .pipe(rename("styles.min.css"))
     .pipe(gulp.dest("dist/styles"));
 }
-
 function js() {
   return gulp
     .src("src/scripts/dev/*.js")
@@ -92,20 +88,19 @@ function js() {
     )
     .pipe(gulp.dest("dist/scripts"));
 }
-
 function jsCopy() {
-  return gulp.src(resources.jsVendor).pipe(plumber()).pipe(gulp.dest("dist/scripts"));
+  return gulp
+    .src(resources.jsVendor)
+    .pipe(plumber())
+    .pipe(gulp.dest("dist/scripts"));
 }
-
 function copy() {
   return gulp
     .src(resources.static, {
-      base: "src",
-      encoding: false
+      base: "src"
     })
     .pipe(gulp.dest("dist/"));
 }
-
 function images() {
   return gulp
     .src(resources.images, { encoding: false })
@@ -113,12 +108,11 @@ function images() {
       imagemin([
         imagemin_gifsicle({ interlaced: true }),
         imagemin_mozjpeg({ quality: 100, progressive: true }),
-        imagemin_optipng({ optimizationLevel: 5 })
+        imagemin_optipng({ optimizationLevel: 3 })
       ])
     )
     .pipe(gulp.dest("dist/assets/images"));
 }
-
 function svgSprite() {
   return gulp
     .src(resources.svgSprite)
@@ -137,14 +131,20 @@ function svgSprite() {
     .pipe(rename("symbols.svg"))
     .pipe(gulp.dest("dist/assets/icons"));
 }
-
-const build = gulp.series(clean, copy, includeHtml, style, js, jsCopy, images, svgSprite);
-
+const build = gulp.series(
+  clean,
+  copy,
+  includeHtml,
+  style,
+  js,
+  jsCopy,
+  images,
+  svgSprite
+);
 function reloadServer(done) {
   server.reload();
   done();
 }
-
 function serve() {
   server.init({
     server: "dist"
@@ -157,7 +157,17 @@ function serve() {
   gulp.watch(resources.images, { delay: 500 }, gulp.series(images, reloadServer));
   gulp.watch(resources.svgSprite, gulp.series(svgSprite, reloadServer));
 }
-
 const start = gulp.series(build, serve);
-
-export { clean, copy, includeHtml, style, js, jsCopy, images, svgSprite, build, serve, start };
+export {
+  clean,
+  copy,
+  includeHtml,
+  style,
+  js,
+  jsCopy,
+  images,
+  svgSprite,
+  build,
+  serve,
+  start
+};
